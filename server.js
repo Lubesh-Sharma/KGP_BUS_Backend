fb10ef2835011fcba15f9b981930b4179e10b460
Lubesh-Sharma <lubesh@kgpian.iitkgp.ac.in>
2025-04-14 03:47:54 +0530

    sjdhhshgdsghdgs

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(
 );
 
 // Handle preflight OPTIONS requests
-app.options('*', cors());
+// app.options('*', cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
