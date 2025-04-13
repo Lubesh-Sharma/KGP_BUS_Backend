de29d8bd379744cda218dae51722b6fe62c61a35
Lubesh-Sharma <lubesh@kgpian.iitkgp.ac.in>
2025-04-14 04:32:37 +0530

    sjdhhshgdsghdgs

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 
 // Middleware to set headers
 app.use((req, res, next) => { 
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin'); 
+  res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none'); 
   res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none'); 
   next(); 
 });
