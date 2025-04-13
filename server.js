5b6574481245e343967c221d2413e83cac352f40
Lubesh-Sharma <lubesh@kgpian.iitkgp.ac.in>
2025-04-14 05:27:39 +0530

    sjdhhshgdsghdgs

--- server.js
+++ server.js
@@ -20,12 +20,28 @@ import profileRouter from "./routers/profile.router.js";
 dotenv.config();
 const app = express();
 
-// // Middleware to set headers
-// app.use((req, res, next) => { 
-//   res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none'); 
-//   res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none'); 
-//   next(); 
-// });
+// Global middleware to handle CORS headers
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+const handler = (req, res) => {
+  const d = new Date();
+  res.end(d.toString());
+};
+
+app.use(allowCors((req, res, next) => next()));
 
 // Connect to PostgreSQL
 connectDB();
